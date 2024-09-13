8da1ce9f2b3eed8939b6fea404e0214ecb722231
Heela Bangash <heelabangash@gmail.com>
2024-09-12 22:05:50 -0400

    add streaming site details structure and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-
-import './App.scss'
-import Button from './components/Button/Button'
-import NavBarBundle from './components/NavBar/NavBarBundle/NavBarBundle'
+import "./App.scss";
+import Button from "./components/Button/Button";
+// import NavBarBundle from "./components/NavBar/NavBarBundle/NavBarBundle";
+// import StreamingSiteDetails from "./components/StreamingSiteDetails/StreamingSiteDetails";
 
 function App() {
-
-  return (
-    <div>
-      <NavBarBundle />
-    </div>
-  )
+	return <div></div>;
 }
 
-export default App
+export default App;
