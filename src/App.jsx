60680c5247a97c3ab474567ecc38c701ae1f9be6
Liz <carriezha321@gmail.com>
2024-09-12 18:04:52 -0700

    add bundle details

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 
 import './App.scss'
 import Button from './components/Button/Button'
+import BundleDetails from './components/BundleDetails/BundleDetails'
+import BundleData from './data/data.json'
+import {useState} from 'react';
+
 
 function App() {
+  const [selectBundle, setSelectBundle] = useState('basic');
+  const handleBundleChange = (event) =>{
+    setSelectBundle(event.targer.value)
+  }
+
+
 
   return (
     <div>
+      <BundleDetails bundleObject = {BundleData.bundles.premium} />
+      {/* {selectedBundle === 'basic' && (
+        <BundleDetails bundleObject={bundleData.bundles.basic} />
+)}
+
+{selectedBundle === 'premium' && (
+  <BundleDetails bundleObject={bundleData.bundles.premium} />
+)} */}
     </div>
   )
 }
