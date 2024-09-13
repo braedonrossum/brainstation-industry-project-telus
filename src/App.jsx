2d8a8294badfb58f62a4dead1d1643aebdb64c10
Newiq <784751939@qq.com>
2024-09-12 22:33:35 -0400

    add nav-bundle

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Button from './components/Button/Button'
 import BundleDetails from './components/BundleDetails/BundleDetails'
 import BundleData from './data/data.json'
 import { useState } from 'react';
+import NavBarBundle from './components/NavBar/NavBarBundle/NavBarBundle';
 
 import NavBarBundleRender from './components/NavBarRender/NavBarBundleRender'
 import NavBarStreamSiteRender from './components/NavBarRender/NavBarStreamSiteRender'
@@ -18,13 +19,14 @@ function App() {
 
   return (
     <div>
+      <NavBarBundle />
       {/* <NavBarBundleRender />
       <NavBarStreamSiteRender />
       <BundleDetails bundleObject={BundleData.bundles.premium} /> */}
       {/* {selectedBundle === 'basic' && (
         <BundleDetails bundleObject={bundleData.bundles.basic} />
       )}
-
+        
       {selectedBundle === 'premium' && (
         <BundleDetails bundleObject={bundleData.bundles.premium} />
       )} */}
