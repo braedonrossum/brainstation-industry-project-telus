f887a55700fb4bfddf906cd78a47a6c8ddec1ea9
Heela Bangash <heelabangash@gmail.com>
2024-09-13 12:08:18 -0400

    extract bundle details from Nav Bar and move to site details

--- src/components/BundleDetails/BundleDetails.jsx
+++ src/components/BundleDetails/BundleDetails.jsx
@@ -1,23 +1,22 @@
 import "./BundleDetails.scss";
 
-function BundleDetails({bundleObject}){
+function BundleDetails({ bundleObject }) {
+	const planDescription = bundleObject["plan-description"];
+	const descriptionItems = Array.isArray(planDescription)
+		? planDescription
+		: [planDescription];
 
-    const planDescription = bundleObject["plan-description"];
-    const descriptionItems = Array.isArray(planDescription)
-    ? planDescription
-    : [planDescription];
-
-    return(
-        <div className="bundle-details">
-            {/* <h2> Details </h2> */}
-            <ul>
-                {descriptionItems.map(
-                (point, index)=> (
-                    <li className="bundle-details__info" key={index}>{point}</li>
-                ))}
-            </ul>
-        </div>
-    )
+	return (
+		<div className="bundle-details">
+			<ul>
+				{descriptionItems.map((point, index) => (
+					<li className="bundle-details__info" key={index}>
+						{point}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default BundleDetails;
--- src/components/StreamingSiteRender/StreamingSiteRender.jsx
+++ src/components/StreamingSiteRender/StreamingSiteRender.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react';
-import NavBarStreamSiteRender from '../NavBarRender/NavBarStreamSiteRender'
-import StreamingSiteDetails from '../StreamingSiteDetails/StreamingSiteDetails'
+import { useState } from "react";
+import NavBarStreamSiteRender from "../NavBarRender/NavBarStreamSiteRender";
+import StreamingSiteDetails from "../StreamingSiteDetails/StreamingSiteDetails";
 
-export default function StreamingSiteRender() {
-    const streams = ['netflix', 'Disney+', 'Amazon', 'Telus'];
-    const [site, setSite] = useState(streams[0]);
+export default function StreamingSiteRender({ currentBundle }) {
+	const streams = ["netflix", "Disney+", "Amazon", "Telus"];
+	const [site, setSite] = useState(streams[0]);
 
-    return (
-        <div>
-            <NavBarStreamSiteRender  setSite={setSite}/>
-            <StreamingSiteDetails site={site}/>
-        </div>
-    )
+	return (
+		<div>
+			<NavBarStreamSiteRender setSite={setSite} />
+			<StreamingSiteDetails plan={currentBundle} site={site} />
+		</div>
+	);
 }
